35156025dee13346ddf4f48a0718c88dbdf7e169
Meenu V Nair <meenu.vnair12@gmail.com>
2025-08-05 08:18:10 +0530

    problems

---
+++ src/main/java/com/meenu/coding/easy/ContainsDuplicateII.java
@@ -0,0 +1,64 @@
+package com.meenu.coding.easy;
+
+
+/**
+ * @author Meenu V. Nair
+ * @created 05/08/25
+ */
+
+/*******  APPROACH ******************** 
+
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**** COMPLEXITY ANALYSIS ********************
+ Time Complexity : O(n)
+ Space Complexity : O(n)
+ */
+
+public class ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])) {
+                int j = map.get(nums[i]);
+                if(Math.abs(i - j) <= k) {
+                    return true;
+                } else {
+                    map.put(nums[i], i);
+                }
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+}
+
+/*******  PROBLEM DESCRIPTION ******************** 
+ Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+
+
+ Example 1:
+
+ Input: nums = [1,2,3,1], k = 3
+ Output: true
+ Example 2:
+
+ Input: nums = [1,0,1,1], k = 1
+ Output: true
+ Example 3:
+
+ Input: nums = [1,2,3,1,2,3], k = 2
+ Output: false
+
+
+ Constraints:
+
+ 1 <= nums.length <= 105
+ -109 <= nums[i] <= 109
+ 0 <= k <= 105
+ */
